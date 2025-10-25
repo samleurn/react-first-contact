57da27d4099a1871fade6776034136e11659fd84
Samleurn <samleudev@gmail.com>
2025-10-25 20:38:00 -0300

    update: estilizando o card

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,32 @@
+import '../css/app.css'
+import { useState } from 'react'
+
 export const App = () => {
-  return <p>Hello World</p>
+  const [n, func] = useState(0)
+  return (
+    <>
+      <div className="card">
+        <div className="header">
+          <div className="dots">
+            <span className="red"></span>
+            <span className="yellow"></span>
+            <span className="green"></span>
+          </div>
+          <p>Click in Hello World</p>
+        </div>
+        <div className="button_count">
+          <button
+            onClick={() => {
+              return func(n + 1)
+            }}
+            className="button-hello"
+          >
+            Hello World
+          </button>
+
+          <p>{n}</p>
+        </div>
+      </div>
+    </>
+  )
 }
