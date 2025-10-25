10f4eb7106e3c78cfea61fee16c0606338909e21
Samleurn <samleudev@gmail.com>
2025-10-25 19:50:01 -0300

    update: estilizando hello world

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app'
+import '../css/layout.css'
 
 createRoot(document.querySelector('#app')!).render(
   <StrictMode>
